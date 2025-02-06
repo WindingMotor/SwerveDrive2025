3d9b60bf7287ec47d2f7cfe58ac4a0c8b56ae12a
WindingMotor <kid.subudhi@gmail.com>
2025-02-06 11:22:20 -0500

    Vendordep rollback

--- src/main/java/frc/robot/constants/BuildConstants.java
+++ src/main/java/frc/robot/constants/BuildConstants.java
@@ -12,12 +12,12 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "SwerveDrive2025";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 62;
-	public static final String GIT_SHA = "1bf8435e78b07bc47217b64b129eb916bd918f04";
-	public static final String GIT_DATE = "2025-02-05 14:43:07 EST";
+	public static final int GIT_REVISION = 65;
+	public static final String GIT_SHA = "ba39520dc48d4ad53073d889ab85ba98750c1d34";
+	public static final String GIT_DATE = "UNKNOWN";
 	public static final String GIT_BRANCH = "main";
-	public static final String BUILD_DATE = "2025-02-05 20:55:00 EST";
-	public static final long BUILD_UNIX_TIME = 1738806900758L;
+	public static final String BUILD_DATE = "2025-02-06 11:22:01 EST";
+	public static final long BUILD_UNIX_TIME = 1738858921208L;
 	public static final int DIRTY = 1;
 
 	private BuildConstants() {}
